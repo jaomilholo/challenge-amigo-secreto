012161763e11e0f74dedc63a260999e1dc8f0d81
Joao Milholo <joaomilholo12@gmail.com>
2025-01-17 21:13:13 -0300

    adicionado a funcao para adicioanr amigo na lista

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-let amigos = []
+let amigos = [];
+
+function adicionarAmigo() { // <- Função para adicionar um amigo
+  let novoAmigo = document.getElementById('amigo').value;
+  if (novoAmigo == '') {
+    alert('Digite o nome para adicionar um amigo.');
+  } else {
+    amigos.push(novoAmigo);
+    console.log(amigos)
+  }
+}
